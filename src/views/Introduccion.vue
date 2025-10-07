b306138ac9dfe125c24eb3b1eb62ded6dd104560
Andrea Botello <andrea.botellor@misena.edu.co>
2025-10-07 18:55:45 -0500

    Ajuste Vinculacion [2.0]

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,7 @@
             .col-lg-12
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/Ts1f1pJU4gM?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/au_TAtSplnA?si=wPULqFab55l_fOiU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
               figcaption #[strong Video.] Más allá del sabor: prevención y seguridad en productos cárnicos
 
 
